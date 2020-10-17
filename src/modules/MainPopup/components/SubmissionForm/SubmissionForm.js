e04c503a3d2c4cfdbc5ee8bad921757d8b340e54
kubapi <kuba.sliz@hotmail.com>
2020-10-17 21:39:46 +0200

    adding .gif banner, margin-top and link

--- src/modules/MainPopup/components/SubmissionForm/SubmissionForm.js
+++ src/modules/MainPopup/components/SubmissionForm/SubmissionForm.js
@@ -8,8 +8,7 @@ import { useTranslation } from "react-i18next";
 import "./SubmissionForm.scss";
 
 // adding FakeHunter challenge banner
-import Banner from "../../../../assets/images/test_banner.png";
-console.log(Banner);
+import Banner from "../../../../assets/images/banner.gif";
 
 const SubmissionForm = ({ handleSubmit, upload }) => {
   const { t, i18n } = useTranslation("popup");
@@ -82,7 +81,9 @@ const SubmissionForm = ({ handleSubmit, upload }) => {
               {t("recaptcha_terms")}
             </a>
           </small>
-          <img src={Banner} alt="Banner" height="50px"/>
+          <a href="https://fakehunter.pap.pl/challenge" target='_blank'>
+            <img className = "banner" src={Banner} alt="Banner" height="50px"/>
+          </a>
         </Form>
       )}
     </Formik>
